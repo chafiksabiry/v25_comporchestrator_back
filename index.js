1ca141fe41e9b503bc8d76817a5c4e3433d7ce28
karouma-oum <omaimaayoube149@gmail.com>
2025-04-11 12:58:28 +0100

    update docker file

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
